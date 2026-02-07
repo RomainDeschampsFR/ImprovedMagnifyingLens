76229b7776afcbb5587428d1d6a1fdf3a50dde70
romainD <romain.deschamps61@sfr.fr>
2026-02-07 13:32:46 +0100

    Update TLD 2.51

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ImprovedMagnifyingLens")]
 [assembly: AssemblyCopyright("Romain")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: MelonInfo(typeof(ImprovedMagnifyingLens.Main), "ImprovedMagnifyingLens", "1.0.0", "Romain")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: MelonInfo(typeof(ImprovedMagnifyingLens.Main), "ImprovedMagnifyingLens", "1.0.1", "Romain")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
